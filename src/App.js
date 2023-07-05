aa65833208cd30d935d3a722c92a2f531ad34674
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-07-05 20:25:55 +0300

    manifest test 4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles/schedule.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/scroll_to_top/ScrollToTop";
+import { useEffect } from "react";
 
 import Header from "./components/header/Header";
 import Navbar_mobile from "./components/nav_bar_mobile/Navbar_mobile";
@@ -14,6 +15,31 @@ import DeadlineTablet from "./components/deadline_block/DeadlineTablet";
 import Sky from "./components/sky/Sky";
 
 function App() {
+  
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    const manifest = document.querySelector('link[rel="manifest"]');
+
+    const setThemeColor = (isDarkMode) => {
+      const themeColor = isDarkMode ? '#000000' : '#EDEDED';
+      if (manifest) {
+        const parsedManifest = JSON.parse(manifest.getAttribute('href'));
+        parsedManifest.theme_color = themeColor;
+        manifest.setAttribute('href', JSON.stringify(parsedManifest));
+      }
+    };
+
+    setThemeColor(prefersDarkMode.matches); // установка изначального значения
+
+    prefersDarkMode.addEventListener('change', (event) => {
+      setThemeColor(event.matches);
+    });
+
+    return () => {
+      prefersDarkMode.removeEventListener('change', setThemeColor);
+    };
+  }, []);
+
   return (
     <div className="App">
       <Router>
@@ -25,17 +51,17 @@ function App() {
           <Header />
           <main className="content_container">
 
-          <Routes>
-           
+            <Routes>
+
               <Route path="/" element={<HomePage />} />
               <Route path="/menu" element={<MenuPage />} />
               <Route path="/course" element={<CoursePage />} />
               <Route path="/courses" element={<CoursesTablet />} />
               <Route path="/deadlines" element={<DeadlineTablet />} />
 
-          </Routes>
+            </Routes>
 
-        </main>
+          </main>
         </div>
 
         <Navbar_mobile />
