4ab8fb8d74376280bea301d9daea200b4abb8f17
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-08-22 02:03:39 +0300

    shop update card

--- src/components/bonuses_block/spend/ShopCard.jsx
+++ src/components/bonuses_block/spend/ShopCard.jsx
@@ -1,7 +1,19 @@
+import { CaretLeft } from '@phosphor-icons/react'
+import { useNavigate } from 'react-router-dom'
+
 const ShopCard = ({ shop }) => {
+	const navigate = useNavigate()
 	return (
-		<div className='block' style={{ padding: '0' }}>
-			<img src={shop.image} alt={shop.name} className='bonus-preview-img' />
+		<div className='block bonus-block-card'>
+			<div className='bonus-back-button' onClick={() => navigate(-1)}>
+				<CaretLeft weight='bold' className='icon_big' />
+				<h4>Назад</h4>
+			</div>
+			<img src={shop.image} alt={shop.name} className='bonus-card-big-img' />
+			<div className='bonus-block bonus-card-wrapper'>
+				<h2>{shop.name}</h2>
+				<pre>{shop.desc}</pre>
+			</div>
 		</div>
 	)
 }
--- src/components/bonuses_block/spend/ShopFullPreview.jsx
+++ src/components/bonuses_block/spend/ShopFullPreview.jsx
@@ -1,9 +1,10 @@
 import { NavLink } from 'react-router-dom'
 
 const ShopFullPreview = ({ shop }) => {
+	
 	return (
 		<NavLink to={`./${shop.id}`} key={shop.id} className='big-item-wrapper'>
-			<img src={shop.image} alt={shop.name} className='bonus-preview-img' />
+			<img id="shopImg" src={shop.image} alt={shop.name} className='bonus-preview-img' />
 			<div className='info-wrapper'>
 				<h4>{shop.name}</h4>
 			</div>
